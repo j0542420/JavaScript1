b4d84633f190d9bed89848481c5c60a1bc6e66b9
Jose Villa <j0542420@amarillocollege.com>
2025-03-17 16:55:53 -0500

    update

---
+++ script.js
@@ -0,0 +1,12 @@
+    // display alert good afternoon
+    window.alert("Good Afternoon");
+    //declare the variable to hold the div element
+    let container = document.getElementById("container");
+    //getElementById is the method to locate the element by the ID
+    // container is an element, is a div in this case
+    // set the content of element  
+    container.innerHTML = "This is the container place";
+    //change color style to red
+    container.style.color = "red";
+    //add new wlwmwnt p into the page
+    document.write("<p>This is new paragraph<p>");
