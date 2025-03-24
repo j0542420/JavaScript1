248f769dcba83fd1ee4a71c165595ad701701635
Jose Villa <j0542420@amarillocollege.com>
2025-03-24 16:58:27 -0500

    Wk2Pt1

--- script.js
+++ script.js
@@ -21,12 +21,20 @@
     
     //declare a variable to hold button element
     let button = document.getElementById("btnClickMe");
+    //btnClickMe is an HTML element, an object
+    //get ElementById is a method of document object
 
-    btnClickMe.onclick = function(){
+    btnClickMe.oncancel = function(){ // onclick is a property
         // window.alert('Hello');
         header.style.color =  "red";
         footer.style.colot = "blue";
-        container.style.visiability = "hidden";
+        //container.style.visiability = "hidden";
+        img.src = "flower.png";
+        //assign the text to input element
+        username.value = container.innerText; // change value of input text
+        //assign value of input element to out element
+        container.innerText = container.innerText;
+        //container.innerText = "some text";
     }
 //handle mouse over event header
 header.onmouseover = function(){
@@ -42,4 +50,51 @@ header.onmouseout = function(){
 let username = document.getElementById("username");
 username.onchange = function(){
     container.innerHTML += username.value;
+}
+// 4 ways to associate a handler to an events
+
+// 1. inline associate
+//<button id="btnClickMe" onclick = "window.alert('clicked');">Click Me</button>
+
+// 2. associate the onclick to an annonymus function
+//btnClickMe.onClick = function() {
+// window.alert("hello")
+// }
+
+// 3. associate a non-annonymus function to onclick
+
+// function handleClick()
+// {
+//     window.alert('clicked');
+// }
+// btnClickMe.onclick= handleClick;
+
+// 4. user the method
+// document.addEventListener('click', handleClick );
+
+// function handleClick()
+// {
+//     window.alert('clicked');
+// }
+
+// 5. arrow function 
+//btnClickMe.onclick = ()=>(window.alert('clicked');)
+
+//declare global variable
+let x=5;
+let PI = 3.14; // declare global constant
+
+let amptString = "";
+//let specialString = 'I\'m fine'; or
+let specialString = "I'm fine";
+
+btnClickMe.onclick = handleClick2;
+function handleClick()
+{
+    window.alert(x);
+}
+
+function handleClick2()
+{
+    window.alert(y);
 }
